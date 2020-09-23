60fde5129b07a615c625017b51d39ec10ebdfd8d
pedrohfs31 <pedrosampaioepr@gmail.com>
2020-09-23 19:46:58 -0300

    create a meeting with update of availability status

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,2 +1,40 @@
 class MeetingsController < ApplicationController
+  before_action :set_meeting, only: [:show, :destroy]
+
+  def index
+  end
+  def show
+
+  end
+  def new
+    @meeting = Meeting.new
+  end
+
+  def create
+    @meeting = Meeting.new(meeting_params)
+
+    @meeting.user = current_user
+
+    if @meeting.save
+      # redirect_to meeting_path(@meeting), notice: 'meeting was successfully created.'
+      # redirect_to controller: 'availabilities', action: 'destroy', id: @meeting.availability
+      # redirect_to :controller=> 'availabilities', :action=> 'destroy', :id=> @meeting.availability
+      render :show, notice: 'meeting was successfully created.'
+
+    else
+      render :new
+    end
+    # redirect_to :controller=>'availabilities', :action=>'destroy', :id => @meeting.availability
+
+  end
+
+  private
+
+  def set_meeting
+    @meeting = Meeting.find(params[:id])
+  end
+
+  def meeting_params
+    params.require(:meeting).permit(:subject, :availability_id)
+  end
 end
