1a55cfd8cd851ac1a34d297204d95955a3d25df9
pedrohfs31 <pedrosampaioepr@gmail.com>
2020-09-23 16:44:45 -0300

    creating meetings controller

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -10,8 +10,19 @@ class AvailabilitiesController < ApplicationController
   end
 
   def new
-
-    @times = { 8.1 => "08:00", 8.2 => "08:20", 8.3 => "08:40", 9.1 => "09:00", 9.2 => "09:20", 9.3 => "09:40", 10.1 => "10:00", 10.2 => "10:20", 10.3 => "10:40", 11.1 => "11:00", 11.2 => "11:20", 11.3 => "11:40", 12.1 => "12:00", 12.2 => "12:20", 12.3 => "12:40", 13.1 => "13:00", 13.2 => "13:20", 13.3 => "13:40", 14.1 => "14:00", 14.2 => "14:20", 14.3 => "14:40", 15.1 => "15:00", 15.2 => "15:20", 15.3 => "15:40", 16.1 => "16:00", 16.2 => "16:20", 16.3 => "16:40", 17.1 => "17:00", 17.2 => "17:20", 17.3 => "17:40", }
+    @times = {}
+    (8..17).to_a.each do |hour|
+      (1..3).to_a.each do |period|
+        case period
+        when 1
+         @times["#{hour}.#{period}".to_f] = "#{sprintf("%02d", hour)}:00"
+        when 2
+          @times["#{hour}.#{period}".to_f] = "#{sprintf("%02d", hour)}:20"
+        else
+          @times["#{hour}.#{period}".to_f] = "#{sprintf("%02d", hour)}:40"
+        end
+      end
+    end
     @availability = Availability.new
 
     # @times =
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :availabilities
+  resources :availabilities, except: [:edit, :update]
+  resources :meetings, only: [:new]
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
