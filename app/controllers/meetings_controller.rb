e3330aae019a3ae2566469be85c0a86aa85de979
Thiago Barros <thiagophilippe@hotmail.com>
2020-09-25 15:50:54 -0300

    controle do botao criar meeting

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -14,18 +14,20 @@ class MeetingsController < ApplicationController
     @meeting = Meeting.new(meeting_params)
 
     @meeting.user = current_user
-
-    if @meeting.save
-      # redirect_to meeting_path(@meeting), notice: 'meeting was successfully created.'
-      # redirect_to controller: 'availabilities', action: 'destroy', id: @meeting.availability
-      # redirect_to :controller=> 'availabilities', :action=> 'destroy', :id=> @meeting.availability
-      render :show, notice: 'meeting was successfully created.'
-
+    if @meeting.availability.user == @meeting.user
+      redirect_to :root, alert: 'This time is yours'
     else
-      render :new
+      if @meeting.save
+        # redirect_to meeting_path(@meeting), notice: 'meeting was successfully created.'
+        # redirect_to controller: 'availabilities', action: 'destroy', id: @meeting.availability
+        # redirect_to :controller=> 'availabilities', :action=> 'destroy', :id=> @meeting.availability
+        render :show, notice: 'meeting was successfully created.'
+
+      else
+        render :new
+      end
     end
     # redirect_to :controller=>'availabilities', :action=>'destroy', :id => @meeting.availability
-
   end
 
   private
